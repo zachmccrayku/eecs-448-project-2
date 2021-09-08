5de4ab56d43d04f6077ffd3d1e3d2bdfbf361ca2
Eric1833 <qq649486419@gmail.com>
2021-09-08 18:27:08 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,3 +7,26 @@ int main()
 
   return(0);
 }
+void run()
+{
+  while(1)
+  {
+    int choice;
+    cout << "1. Start" << endl;
+    cout << "2. Setting" << endl;
+    cout << "3. Quit" << endl;
+    cin >> choice;
+    if(choice ==1)
+    { 
+      letsPlay();
+    }
+    if(choice ==2)
+    { 
+      setting();
+    }
+    if(choice ==3)
+    {
+      break;
+    }
+  }
+}  
