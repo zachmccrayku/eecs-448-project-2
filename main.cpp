d2590ca8b6c4b4584c3404b2058af4262be2b4bb
reganjanssen <89795866+reganjanssen@users.noreply.github.com>
2021-09-08 21:43:23 -0500

    Added documentation

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/**
+  File Name: main.cpp
+  Authors: Regan Janssen, Andrew Loaiza, Chen Lu, Jui Nagarkar, Natasha Shirley
+  Description: main class to initiate the program
+  Date: Sept. 19, 2021
+  */ 
+
 #include "UserInteraction.h"
 
 int main()
