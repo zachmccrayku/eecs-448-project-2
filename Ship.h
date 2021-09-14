6490342b29ac09aa9040abf3153b5e5f5a963720
reganjanssen <89795866+reganjanssen@users.noreply.github.com>
2021-09-14 14:52:47 -0500

    documentation updates

--- Ship.h
+++ Ship.h
@@ -8,61 +8,65 @@ class Ship
 {
   public:
    /*
-   **Sets m_ship, m_hasBeenHit to false, m_isHorizontal to true, gridChar to '_'
+   *Sets m_ship, m_hasBeenHit to false, m_isHorizontal to true, gridChar to '_'
    *@param None
    */
     Ship(); 
 
     /*
-    **Deletes ship
+    *Deletes ship
     *@param None
     */
     ~Ship(); 
 
     /*
-	  **Changes to ^ when it is placed, changes to X when it is hit
+	  *Changes to ^ when it is placed, changes to X when it is hit
 	  *@param None
+    *@returns character value based on hit or placement
 	  */
     char gridChar; 
 
     /*
-    **Checks to see whether or not the coordinate contains a ship
+    *Checks to see whether or not the coordinate contains a ship
 	  *@param None
+    *@return boolean value for coordinate
     */
     bool m_isShip; 
 
     /*
-	  **Checks to see whether or not the ship has been hit 
+	  *Checks to see whether or not the ship has been hit 
 	  *@param None
+    *@return boolean value for ship status
 	  */
     bool m_hasBeenHit; 
 
     /*
-	  **Sets a ships placement to either horizontal or vertical
+	  *Sets a ships placement to either horizontal or vertical
     *@param None
+    *@returns boolean value for horizontal or vertical
     */
     bool m_isHorizontal; 
 
     /*
-	  **Checks if there is a ship, if not it makes is_ship true 
+	  *Checks if there is a ship, if not it makes is_ship true 
 	  *@param None
 	  */
     void placeShip(); 
 
     /*
-	  **Marks the ship as hit
+	  *Marks the ship as hit
 	  *@param None
 	  */ 
     void hitShip(); 
 
     /*
-	  **Checks to see if the ship has been sunk
+	  *Checks to see if the ship has been sunk
 	  *@param None
 	  */ 
     void isSunk(); 
 
     /*
-	  **Changes state of m_isHorizontal 
+	  *Changes state of m_isHorizontal 
 	  *@param None
 	  */ 
     void makeHorizontal();  
--- UserInteraction.h
+++ UserInteraction.h
@@ -9,17 +9,42 @@ using namespace std;
 class UserInteraction
 {
 private:
-  Board* player1Board;
-  Board* player2Board;
-  GamePlay choice;
-  int numShips;
+  Board* player1Board; // Points to player 1 board
+  Board* player2Board; // Points to player 2 board
+  GamePlay choice; // Gives players the gameplay options
+  int numShips; // Is given by user and sets up the number of ships for placement
 
 public:
-  UserInteraction(); // initialize a board for each player
+  /*
+   *Initalizes a board for each player
+   *@param None
+   */
+  UserInteraction();
+
   ~UserInteraction(); // avoid memory leaks!
-  void playGame(); // run calls play game, this is where it all runs
-  void run(); // is called from main.cpp
+
+  /*
+   *Run function calls playgame which contains the main game functionality
+   *@param None
+   */
+  void playGame(); 
+
+  /*
+   *Is called from main.cpp to start the game
+   *@param None
+   */
+  void run(); 
+
+  /*
+   *Allows players to view their board as well as the opponents board with their hits and misses
+   *@param None
+   */
   void viewBoard();
+
+  /*
+   *Ship is placed on the board 
+   *@param tempboard- allows for editing of player 1 and player 2 boards
+   */
   void shipPlacement(Board* tempBoard);
 };
 
