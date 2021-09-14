da50c4947a8b18dbb3925a18bdda533c2f03e9fe
Eric1833 <qq649486419@gmail.com>
2021-09-14 13:14:21 -0500

    Update UserInteraction.cpp

--- UserInteraction.cpp
+++ UserInteraction.cpp
@@ -4,6 +4,8 @@ UserInteraction::UserInteraction()
 {
   player1Board = new Board();
   player2Board = new Board();
+  //player1Ship = new array [][];
+  //player2Ship = new araay [][];
 }
 
 UserInteraction::~UserInteraction()
@@ -26,7 +28,25 @@ void UserInteraction::playGame()
     if(choice == 1)
     {
       cout << "Game is starting...\n";
+      cout << "***************************"; 
+      cout << "Player 1";
+      cout << "***************************"; 
       player1Board->viewBoard();
+      cout << "input your number of ships ";
+      int cout;
+      cin >> cout;
+      for(int k=0; k<=cout; k++)
+      {
+          //set each ship  
+          //if()
+          cout << "Player 1 what you like to set your ship input your col and row";
+      }
+      //after set player 1 and then go to player 2 
+      
+
+
+
+
     }
     if(choice == 2)
     {
