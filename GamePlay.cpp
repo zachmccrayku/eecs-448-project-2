8b21e847d147ef0db7972262f8ea9c7541ce54de
njshirley <89923687+njshirley@users.noreply.github.com>
2021-09-16 15:59:50 -0500

    finished isSunk

--- GamePlay.cpp
+++ GamePlay.cpp
@@ -7,45 +7,13 @@ GamePlay::GamePlay()
 
 GamePlay::~GamePlay(){}
 
-// void GamePlay::fireAt(Board* CurrentBoard)
-int GamePlay::fireAt()
+void GamePlay::fireAt(Board* CurrentBoard)
 {
-  // do
-  // {
-  //   cout << "Where would you like to fire?";
-  //   cin >> fireAtSpot;
-  //   convertString(fireAtSpot);
-  //  if(fireSpotNum >= 49 || fireSpotNum <= 57)
-  //   {
-  //     fireSpotNum = fireSpotNum - 49;
-  //       if(fireSpotLetter >= 65 || fireSpotLetter <= 74)
-  //       {
-  //         fireSpotLetter= fireSpotLetter - 65;
-  //
-  //         }
-  //       else if(fireSpotLetter >= 97 || fireSpotLetter <= 106)
-  //       {
-  //          fireSpotLetter= fireSpotLetter - 97;
-  //       }
-  //   }
-   //       if(currentBoard[fireSpotNum][fireSpotLetter].getChar() == "_")
-  //       {
-  //          canBeFired==True;
-  //       }
-  //
-  //
-  //   else
-  //   {
-  //     cout << "Error! Try Again!";
-  //    }
-  //
- //
-  //  }
-  //    while(canBeFired == false);
-  //  CurrentBoard[i][j].hitShip();
+
+  //    CurrentBoard[i][j].hitShip();
   //
 
-}
+//      }
 
 bool GamePlay::isSunk()
 {
@@ -67,9 +35,98 @@ bool GamePlay::quitGame()
   // }
 }
 
-int GamePlay::convertString(string temp)
+void GamePlay::convertString(string temp)
 {
-  // fireSpotLetter = int(temp.at(0));
-  //
-  // fireSpotNum = int(temp.at(1));
+
+    // do
+  // {
+  //   cout << "Where would you like to fire?";
+  //   cin >> fireAtSpot;
+  //   if(fireAtSpot.length() == 2)
+  //   { 
+  //     fireSpotLetter = int(temp.at(0));
+  // 
+  //     fireSpotNum = int(temp.at(1));
+  //    if(fireSpotNum >= 49 || fireSpotNum <=57)
+  //    {
+  //      fireSpotNum = fireSpotNum-49;
+  //       if(fireSpotLetter >= 65 || fireSpotLetter <= 74)
+  //       {
+  //         fireSpotLetter= fireSpotLetter - 65;
+  //         if(currentBoard[fireSpotNum][fireSpotLetter].getChar() == "_")
+  //         {
+  //          canBeFired==True;
+  //         }
+  //        }
+  //       else if(fireSpotLetter >= 97 || fireSpotLetter <= 106)
+  //       {
+  //          fireSpotLetter= fireSpotLetter - 97;
+  //         if(currentBoard[fireSpotNum][fireSpotLetter].getChar() == "_")
+  //         {
+  //          canBeFired==True;
+  //         }
+  //       }
+  //    }
+  //   else
+  //   {
+  //     cout << "Error! Try Again!";
+  //    }
+  //  }
+  //    while(canBeFired == false);
+}
+
+bool Board::isSunk( int row, int col)
+{
+      if(m_grid[row][col].isHorizontal == true)
+      {
+        for(int i = col; i < numCols; i++)
+        {
+          if(m_grid[row][i].getChar() == '^')
+          {
+            return (false)
+           }
+          if(m_grid[row][i].getChar() == '_')
+          {
+            if(m_grid[row][i-1].getChar() == '^')
+            {
+              return(false); 
+            }
+            else if(i-1 != 0)
+            {
+             isSunk(m_grid, row, i-1);
+            }
+            else if(m_grid[row][i-1].getChar() == 'X')
+            {
+              return(true);
+            }
+          }
+          break;
+        }
+      }
+      else
+      {
+        for(int i = row; i < numRows; i++)
+        {
+          if(m_grid[i][col].getChar() == '^')
+          {
+            return (false)
+           }
+          if(m_grid[i][col].getChar() == '_')
+          {
+            if(m_grid[i-1][col].getChar() == '^')
+            {
+              return(false); 
+            }
+            else if(i-1 != 0)
+            {
+             isSunk(m_grid, i-1, col);
+            }
+            else if(m_grid[i-1][col].getChar() == 'X')
+            {
+              return(true);
+            }
+          }
+          break;
+        }
+      }       
 }
