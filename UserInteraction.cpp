451f17099dcee8dc7891f68572f18e31ad53c526
Andrew Loaiza <andrewloaiza@ANDREW-MacBook-Pro.local>
2021-09-16 15:41:10 -0500

    working menu

--- UserInteraction.cpp
+++ UserInteraction.cpp
@@ -22,6 +22,7 @@ void UserInteraction::playGame()
   while(1)
   {
     int choice;
+    p1Turn = true;
     cout << "Hi! Welcome to Battleship!\n";
     cout << "1. Start" << endl; // start the game
     cout << "2. Quit" << endl; //quit the game
@@ -31,7 +32,7 @@ void UserInteraction::playGame()
       cout << "***************************\n";
       cout << "Game is starting...\n";
       cout << "***************************\n";
-      
+
 
       cout << "input your number of ships (enter a value between 1-6): ";
       cin >> numShips;
@@ -42,44 +43,40 @@ void UserInteraction::playGame()
         cin >> numShips;
       }
       while(numShips > 6 || numShips <=0);
-      
 
-     cout << "Player 1 where you like to set your ship?\n";
+      cout << "Player 1 where you like to set your ship?\n";
       player1->shipPlacement(numShips);
-     cout << "Player 2 where you like to set your ship\n";
+      cout << "Player 2 where you like to set your ship\n";
       player2->shipPlacement(numShips);
 
-    while(numShips != isSunk)   
+      while(1)
       {
-       int choose;
-        
-       cout << "1. Attack opponent\n"; 
-       cout << "2. View my board\n"; 
-       cout << "3. View opponent board\n"; 
-       cout << "4. Quit\n"; 
-       cout << "What would you like to do: \n";
-        cin >> choose;
+        int choose;
         do
         {
-          cout << "Sorry, the value entered was not between the allowed value. Please try again.\n";
-          
-          cout << "1. Attack opponent\n"; 
-          cout << "2. View my board\n"; 
-          cout << "3. View opponent board\n"; 
-          cout << "4. Quit\n"; 
-          cout << "What would you like to do: \n"; 
+          cout << "1. Attack opponent\n";
+          cout << "2. View my board\n";
+          cout << "3. View opponent board\n";
+          cout << "4. Quit\n";
+          cout << "What would you like to do: \n";
           cin >> choose;
+
+          if (choose <= 0 || choose > 4)
+          {
+            cout << "Sorry, the value entered was not between the allowed value. Please try again.\n";
+          }
         } while(choose <= 0 || choose > 4);
-        
 
-       if(choose == 1)
-       {
-         fireAt();
-       }
 
-       if(choose == 2)
-       {
-          if(player1 == true)
+        if(choose == 1)
+        {
+          // fireAt();
+          cout << "fireAt()...";
+        }
+
+        if(choose == 2)
+        {
+          if(p1Turn)
           {
             player1->viewBoard();
           }
@@ -87,31 +84,38 @@ void UserInteraction::playGame()
           {
             player2->viewBoard();
           }
-       }
+        }
 
-       if(choose == 3)
-       {
-          if(player1 == true)
+        if(choose == 3)
+        {
+          if(p1Turn)
           {
-            player1->viewOBoard();
+            // player1->viewOBoard();
+            cout << "under development\n\n";
           }
+
           else
           {
-            player2->viewOBoard();
+            // player2->viewOBoard();
+            cout << "under development\n\n";
           }
-       }
-       if(choose == 4)
-       {
-         cout << "Quitting...";
-         break;
-       }
+        }
+
+        if(choose == 4)
+        {
+          cout << "Quitting...";
+          break;
+        }
+
       }
 
-    }
-    if(choice == 2)
+  }
+
+    if(choice == 2) // if quit
     {
       break;
     }
+
     if(choice<1 || choice>2)
     {
       cout << "Please enter the correct number to start a game or quit!"<< endl;
--- UserInteraction.h
+++ UserInteraction.h
@@ -3,7 +3,7 @@
    Authors: Regan Janssen, Andrew Loaiza, Chen Lu, Jui Nagarkar, Natasha Shirley
    Description: Acts as executive class for the game. Initializes players and boards and runs the game
    Date: Sept. 19, 2021
-   */ 
+   */
 
 #ifndef USERINTERACTION_H
 #define USERINTERACTION_H
@@ -20,6 +20,7 @@ private:
   Board* player2; // Points to player 2 board
   GamePlay choice; // Gives players the gameplay options
   int numShips; // Is given by user and sets up the number of ships for placement
+  bool p1Turn;
 
 public:
   /*
