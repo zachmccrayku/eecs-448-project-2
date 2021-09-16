5379bfbbf0bee18abbeeb528dddf88dcb22ec0bc
Andrew Loaiza <andrewloaiza@ANDREW-MacBook-Pro.local>
2021-09-16 13:55:04 -0500

    temporary commented out

--- GamePlay.cpp
+++ GamePlay.cpp
@@ -9,29 +9,29 @@ GamePlay::~GamePlay(){}
 
 int GamePlay::fireAt()
 {
-  do
-  {
-    cout << "Where would you like to fire?";
-    cin >> fireAtSpot;
-    convertString(fireAtSpot);
-   if(fireSpot1 >= 65 || fireSpot1 <= 74)
-     {
-       fireSpotLetter= fireSpotLetter - 66;
-       canBeFired = true;
-     }
-    else if(fireSpotNum >= 1 || fireSpotNum <= 10)
-    {
-      fireSpotNum = fireSpotNum - 1;
-      canBeFired = true;
-    }
-    else
-    {
-      cout << "Error! Location Does Not Exist! Try Again!";
-     }
-   }
-     while(canBeFired == false);
-    return(fireSpotLetter, fireSpotNum);
-  
+  // do
+  // {
+  //   cout << "Where would you like to fire?";
+  //   cin >> fireAtSpot;
+  //   convertString(fireAtSpot);
+  //  if(fireSpot1 >= 65 || fireSpot1 <= 74)
+  //    {
+  //      fireSpotLetter= fireSpotLetter - 66;
+  //      canBeFired = true;
+  //    }
+  //   else if(fireSpotNum >= 1 || fireSpotNum <= 10)
+  //   {
+  //     fireSpotNum = fireSpotNum - 1;
+  //     canBeFired = true;
+  //   }
+  //   else
+  //   {
+  //     cout << "Error! Location Does Not Exist! Try Again!";
+  //    }
+  //  }
+  //    while(canBeFired == false);
+  //   return(fireSpotLetter, fireSpotNum);
+
 }
 
 bool GamePlay::isSunk()
@@ -40,23 +40,23 @@ bool GamePlay::isSunk()
 }
 
 bool GamePlay::quitGame()
-{ 
-  
-  cout << "Would you like to quit the game? Y/N";
-  cin >> choice;
-  if (choice == 'y' || choice == 'Y')
-  {
-  return(true);
-  }
-  else 
-  {
-    return(false);
-  }
+{
+  //
+  // cout << "Would you like to quit the game? Y/N";
+  // cin >> choice;
+  // if (choice == 'y' || choice == 'Y')
+  // {
+  // return(true);
+  // }
+  // else
+  // {
+  //   return(false);
+  // }
 }
 
 int GamePlay::convertString(string temp)
 {
-  fireSpotLetter = int(temp.at(0));
- 
-  fireSpotNum = int(temp.at(1)); 
+  // fireSpotLetter = int(temp.at(0));
+  //
+  // fireSpotNum = int(temp.at(1));
 }
