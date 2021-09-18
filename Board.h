7110eab922597d3b038a7ad4c33592f4044a3ce6
njshirley <89923687+njshirley@users.noreply.github.com>
2021-09-18 09:50:12 -0500

    Update Board.h

--- Board.h
+++ Board.h
@@ -25,7 +25,7 @@ private:
   int m_col; //current col location
   int m_numShips; //number of ships
   int m_shipsSunk; //number of ships that have been sunk
-  string userInput; //string to store user input
+  string fireAtSpot; //string to store user input
   bool canBeFired; //true if the spot can be fired
 
 public:
