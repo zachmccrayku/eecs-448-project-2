3f017643cec4182b40d06aeed7c5451ef6680032
Max Patiiuk <max@patii.uk>
2021-09-25 18:24:21 -0500

    Remove redundant comment in main.cpp

--- eecs-448-project-2/main.cpp
+++ eecs-448-project-2/main.cpp
@@ -1,10 +1,3 @@
-/**
-  File Name: main.cpp
-  Authors: Regan Janssen, Andrew Loaiza, Chen Lu, Jui Nagarkar, Natasha Shirley
-  Description: Executes the run function and begins the program
-  Date: Sept. 19, 2021
-  */
-
 #include "UserInteraction.h"
 
 int main()
